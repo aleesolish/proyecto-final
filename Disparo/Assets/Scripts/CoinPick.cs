1995931c7944398e1274c6d05fb1982d60fd29ac
aleesolish <aleesolish@gmail.com>
2018-09-07 17:12:44 -0500

    primera fase

---
+++ Disparo/Assets/Scripts/CoinPick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPick : MonoBehaviour {
+
+
+	public int pointsToAdd;
+
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.GetComponent<Player> () == null)
+			return;
+
+		ScoreManager.AddPoints (pointsToAdd);
+
+		Destroy (gameObject);
+	}
+}
---
+++ Disparo/Assets/Scripts/FireBallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallController : MonoBehaviour {
+
+	public float speed;
+
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+		
+		player = FindObjectOfType<Player>();
+
+		if (player.transform.localScale.x < 0)
+			speed = -speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, GetComponent<Rigidbody2D> ().velocity.y); 
+
+	}
+}
